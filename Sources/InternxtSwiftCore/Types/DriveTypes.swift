fbd1aa23db461ee6dddf3118081af19768088d20
patricioxavier8 <116120453+patricioxavier8@users.noreply.github.com>
2026-02-02 08:03:22 -0500

    fix: makes plain name optional

--- Sources/InternxtSwiftCore/Types/DriveTypes.swift
+++ Sources/InternxtSwiftCore/Types/DriveTypes.swift
@@ -205,7 +205,7 @@ public struct GetFolderMetaByIdResponse: Decodable {
     public let deletedAt: String?
     public let removedAt: String?
     public let uuid: String?
-    public let plainName: String
+    public let plainName: String?
     public let removed: Bool?
     public let parentUuid: String?
 }
